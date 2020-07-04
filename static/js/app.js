3bcb97ccbc9dfa53feccc040f5194462e7ece72c
Quinton Wandzura <quinton.wandzura2@ucalgary.ca>
2020-07-04 15:33:08 -0600

    Uploading

--- static/js/app.js
+++ static/js/app.js
@@ -19,5 +19,30 @@ function buildTable(data){
             cell.text(val);
         });
     });
+};
 
-}
+function handleClick(){
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // check to see if a date was entered and filter the 
+    // data using that date.
+    if (date) {
+        // === implies strict equality and == implies loose equality 
+        // Apply 'filter'to the table data to only keep the 
+        // rows where the 'datetime' value matches the filter value 
+        filteredData = filteredData(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data 
+    // @ NOTE: If no date was enterred, then filteredData will 
+    // just be the original tableData.
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button 
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
